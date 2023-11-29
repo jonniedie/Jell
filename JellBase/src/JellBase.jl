f01434c87c61aa2d9dc4e2ead6ff7b95fc2e4650
Jonnie Diegelman <jonniediegelman@gmail.com>
2023-11-28 21:20:28 -0500

    Jongleur -> Jell

---
+++ JellBase/src/JellBase.jl
@@ -0,0 +1,59 @@
+module JellBase
+
+using FunctionWrappers: FunctionWrapper
+using MacroTools: postwalk
+
+export @choose, AbstractCharacter, PlayerChoice, show_dialogue
+
+struct PlayerChoice
+    choice_pairs::Vector{Pair{String, FunctionWrapper{Nothing, Tuple{}}}}
+end
+function PlayerChoice(choices::Vector{Pair{<:AbstractString, <:Function}})
+    # In case the inputs aren't wrapped in a FunctionWrapper already
+    return map(choices) do choice
+        str, fn = choice
+        # We need to make sure the function actually returns nothing
+        fn_nothing = function ()
+            fn()
+            nothing
+        end
+        String(str) => FunctionWrapper{Nothing, Tuple{}}(fn_nothing)
+    end
+end
+
+macro choose(things)
+    things = postwalk(things) do thing
+        if thing isa Expr && thing.head == :call && thing.args[1] == :(=>)
+            thing.args[2] = :(String($(esc(thing.args[2]))))
+            thing.args[3] = :(
+                function ()
+                    $(esc(thing.args[3]))
+                    nothing
+                end
+            )
+        end
+        thing
+    end
+    return :(PlayerChoice($things))
+end
+
+abstract type AbstractCharacter end
+
+function show_dialogue(c::AbstractCharacter, s)
+    @error """
+        No method for `show_dialogue(::$(typeof(c)), ::Any)` implemented.
+        Please make one.
+        """
+end
+function show_dialogue(c::AbstractCharacter, choices::PlayerChoice)
+    @error """
+        No method for `show_dialogue(::$(typeof(c)), ::PlayerChoice)` implemented.
+        Please make one.
+        """
+end
+
+Base.:(:)(c::AbstractCharacter, ::Nothing) = nothing
+Base.:(:)(c::AbstractCharacter, s) = show_dialogue(c, s)
+
+
+end
---
+++ JellTerminal/src/JellTerminal.jl
@@ -0,0 +1,94 @@
+module JellTerminal
+
+using Crayons: Crayon, CrayonWrapper, Box
+using JellBase
+using REPL
+using REPL.TerminalMenus
+using REPL.Terminals
+
+export @choose, Character, Narrator, show_dialogue
+
+@kwdef struct TerminalPrinter
+    color::Symbol = :default
+    sleep_time::Float64 = 0.0
+end
+
+@kwdef struct NamePrinter
+    name::String
+    color::Symbol = :default
+end
+
+struct NoNamePrinter end
+
+@kwdef struct DialoguePrinter
+    color::Symbol = :default
+    sleep_time::Float64 = 0.0
+end
+
+abstract type TerminalCharacter <: AbstractCharacter end
+
+@kwdef struct Character <: TerminalCharacter
+    name::String
+    color::Symbol = :default
+end
+
+struct Narrator <: TerminalCharacter end
+
+function request_continue()
+    # t = TTYTerminal("", stdin, stdout, stderr)
+    # printstyled("\n\n[Press Enter to continue]"; color=:light_black)
+    # readline()
+    # Terminals.cmove_line_up(t)
+    # Terminals.clear_line(t)
+    # Terminals.cmove_line_up(t)
+    # Terminals.clear_line(t)
+    println()
+    sleep(1)
+end
+
+function slow_print(str::String; timer = 0.02, kwargs...)
+    for char in str
+        printstyled(char; kwargs...)
+        sleep(timer)
+    end
+end
+function slow_print(cw::CrayonWrapper; kwargs...)
+    for str in cw.v
+        slow_print(cw.c, str; kwargs...)
+    end
+end
+function slow_print(crayon::Crayon, str::String; timer = 0.02)
+    for char in str
+        print(crayon, char)
+        sleep(timer)
+    end
+end
+function slow_print(crayon::Crayon, cw::CrayonWrapper; kwargs...)
+    print(crayon, cw)
+end
+
+function JellBase.show_dialogue(c::Character, player_choice::PlayerChoice)
+    println()
+    printstyled(c.name; c.color)
+    println(": ")
+    choices = first.(player_choice.choice_pairs)
+    results = last.(player_choice.choice_pairs)
+    choice = request(RadioMenu(choices; scroll_wrap = true, cursor = '▷'))
+    return results[choice]()
+end
+function JellBase.show_dialogue(c::Character, s)
+    println()
+    printstyled(c.name; c.color)
+    print(": ")
+    slow_print(s)
+    request_continue()
+    return nothing
+end
+function JellBase.show_dialogue(c::Narrator, s)
+    println()
+    slow_print(s; color = :italic)
+    request_continue()
+    return nothing
+end
+
+end
