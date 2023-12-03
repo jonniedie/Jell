4d7c1cbf594d2e29098c8746e8d63ad1e705034e
Jonnie Diegelman <jonniediegelman@gmail.com>
2023-12-03 15:22:15 -0500

    Make an environment for the examples folder.

--- examples/custom_characters.jl
+++ examples/custom_characters.jl
@@ -1,19 +1,20 @@
+# To run this example, activate and instantiate the `examples` folder's
+# environment. We'll use the SpelledOut package to spell out numbers as words
+# instead of just printing the number.
 using Jell
+using SpelledOut
 
 # Define a new custom character type. For this example, the character we want
-# to keep track of the word count of the character, so 
+# to keep track of the word count of the character.
 @kwdef mutable struct Character <: AbstractCharacter
     name::String
     word_count::Int = 0
     name_style::Symbol = :default
-    dialogue_style::Symbol = :default
 end
 
 Jell.nameof(c::Character) = c.name
 
 Jell.name_style(c::Character) = c.name_style
-
-Jell.dialogue_style(c::Character) = c.dialogue_style
 
 function Jell.show_dialogue(c::Character, str)
     c.word_count += length(split(str, ' '))
@@ -25,7 +26,7 @@ alice = Character(name="Alice", name_style=:yellow)
 bob = Character(name="Bob", name_style=:blue)
 
 # Run the script
-alice: "Hey Bob. I wanted to try something."
+alice: "Hey, I wanted to try something."
 bob: "Yeah? What's that?"
 alice: "I want you to count how many words I say."
 bob: "Including the ones you've already said?"
@@ -38,6 +39,6 @@ not_included_count = alice.word_count
 alice: "Did you start counting?"
 bob: "Yep."
 alice: "How many have I said so far?"
-bob: "You've said $(alice.word_count - not_included_count) words."
+bob: "You've said $(spelled_out(alice.word_count - not_included_count)) words."
 alice: "That sounds about right."
-bob: "Make that $(alice.word_count - not_included_count)."
+bob: "Make that $(spelled_out(alice.word_count - not_included_count))."
